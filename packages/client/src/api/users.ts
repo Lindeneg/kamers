87152bdc525a8989aab367e69bf753c09cb09a34
lindeneg <christian@lindeneg.org>
2026-03-01 16:20:33 +0100

    allow transfer of tenant ownership

--- packages/client/src/api/users.ts
+++ packages/client/src/api/users.ts
@@ -3,7 +3,7 @@ import api from "./client";
 
 export type User = UserDetail;
 
-export function listUsers(params?: Partial<PaginationParams>) {
+export function listUsers(params?: Partial<PaginationParams> & {tenantId?: string}) {
     return api.get<UsersResponse["list"]>("/users", {params});
 }
 
@@ -13,4 +13,8 @@ export function inviteUser(email: string, name: string) {
 
 export function updateUserPermissions(userId: string, permissions: string[]) {
     return api.put<UsersResponse["updatePermissions"]>(`/users/${userId}/permissions`, {permissions});
+}
+
+export function transferOwnership(userId: string) {
+    return api.put<UsersResponse["transferOwnership"]>(`/users/${userId}/transfer-ownership`);
 }
